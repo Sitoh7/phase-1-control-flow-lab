54851f0efa4fa2520f66a26c13719aea957be57a
Sitoh7 <ha.wakhule@gmail.com>
2024-09-23 12:21:25 +0300

    use conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if(x <= 400){
+    return "This one is on me!";
+  }
+ else if ( x>400 && x<2000){
+  return "That will be twenty bucks.";
+ }
+
+else if (x>2000 && x<2500 ){
+  return "I will gladly take your thirty bucks.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (x > 2500){
+  return "No can do.";
 }
 
-function switchOnCharmFromTip(){
+}
+
+function ternaryCheckCity(y){
   // Write your code here!
+
+ if( y == 'NYC'){
+  return "Ok, sounds good.";
+ }
+
+else{
+  return "No go.";
+}
+
+
+}
+
+function switchOnCharmFromTip(x){
+  // Write your code here!
+
+  if(x == "generous"){
+    return "Thank you so much.";
+  }
+  else if(x == "not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
